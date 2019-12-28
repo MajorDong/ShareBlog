42dcf973af7e66d42c7a4a782d7e6c8a3bc04832
MajorDong <odsts@outlook.com>
2019-12-28 17:39:44 +0800

    创建项目，初始化router配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,55 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/index.vue'
+import Detail from '../views/detail'
+import Create from '../views/create'
+import Edit from '../views/edit'
+import User from '../views/user'
+import My from '../views/my'
+import Login from '../views/login'
+import Register from '../views/register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/detail',
+    name: 'detail',
+    component: Detail
+  },
+  {
+    path: '/create',
+    name: 'cerate',
+    component: Create
+  },
+  {
+    path: '/edit',
+    name: 'edit',
+    component: Edit
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: User
+  },
+  {
+    path: '/my',
+    name: 'my',
+    component: My
+  },{
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
