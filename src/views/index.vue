7a6a03172d1b5f89d99abb8ef03bef0fe23c0acf
MajorDong <odsts@outlook.com>
2019-12-29 13:02:23 +0800

    完成 helpers 中的 request.js

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     首页
+    <el-button @click="onClick1">默认按钮</el-button>
+    <el-button @click="alert">默认按钮</el-button>
   </div>
 </template>
 
 <script>
-export default {
+import request from '../helpers/request.js'
+window.request = request
 
-}
+export default {
+  name: "index",
+  data() {
+    return {};
+  },
+  methods: {
+    onClick1() {
+      this.$message.error("错了哦，这是一条错误消息");
+    },
+    alert() {
+      this.$alert();
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
