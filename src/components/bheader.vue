409712b72319a2ccdfd7001e510f1cf2a03230bd
MajorDong <odsts@outlook.com>
2019-12-31 20:50:56 +0800

    完成登录 注册 功能

--- src/components/bheader.vue
+++ src/components/bheader.vue
@@ -2,22 +2,29 @@
   <div class="bheader">
     <div class="header1" v-if="!isLogin">
       <div class="banner">
-        <div class="logo">
-          <span>BlogTalk</span>
-        </div>
+        <router-link to="/">
+          <div class="logo">
+            <span>BlogTalk</span>
+          </div>
+        </router-link>
         <div class="sloganWrapper">
           <h1>We think through blogs.</h1>
           <div class="line"></div>
           <div class="title">用博客交流思想 LET'S SHARE</div>
           <div>
-            <el-button>立即登录</el-button>
-            <el-button>注册账号</el-button>
+            <router-link to="/login">
+              <el-button>立即登录</el-button>
+            </router-link>
+            <router-link to="/register">
+              <el-button>注册账号</el-button>
+            </router-link>
           </div>
         </div>
       </div>
     </div>
     <div class="header2" v-else>
       <h1>BlOGTALK</h1>
+      
 
       <div>
         <i class="edit el-icon-edit"></i>
@@ -46,15 +53,15 @@ export default {
     return {};
   },
   computed: {
-    ...mapGetters(['user', 'isLogin'])
+    ...mapGetters(["user", "isLogin"])
   },
   created() {
-    this.checkLogin().then;
+    this.checkLogin();
   },
   methods: {
-    ...mapActions(['checkLogin', 'logout']),
-    onLogout(){
-      this.logout()
+    ...mapActions(["checkLogin", "logout"]),
+    onLogout() {
+      this.logout();
     }
   }
 };
@@ -115,70 +122,75 @@ export default {
       }
     }
   }
-  .header1 > .banner {
-    font: 14px/1.6 Arial, "Microsoft YaHei", "黑体", "宋体", sans-serif;
-    height: 470px;
-    width: 100%;
-    background-image: url(../assets/img/bheader.jpg);
-    background-position: center center;
-    background-size: cover;
+  .header1 {
+    height: 400px;
     position: relative;
-    > .logo {
-      text-decoration: none;
-      font-family: "Kaushan Script", "Helvetica Neue", Helvetica, Arial, cursive;
-      font-size: 20px;
-      font-weight: bold;
-      position: absolute;
-      transform: translateY(-50%);
-      color: #fff;
-      top: 50%;
-      left: 0;
-      width: 150px;
-      height: 150px;
-      background-color: #0a1d30;
-      z-index: 30;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    > .sloganWrapper {
-      position: absolute;
-      top: 30%;
-      left: 25%;
-      color: #ffffff;
-      font-family: "Libre Baskerville", serif;
+    > .banner {
+      font: 14px/1.6 Arial, "Microsoft YaHei", "黑体", "宋体", sans-serif;
+      height: 100%;
+      width: 100%;
+      background-image: url(../assets/img/bheader.jpg);
+      background-position: center center;
+      background-size: cover;
 
-      > h1 {
-        font-size: 60px;
-        font-family: "Libre Baskerville", serif;
+      .logo {
+        text-decoration: none;
+        font-family: "Kaushan Script", "Helvetica Neue", Helvetica, Arial,
+          cursive;
+        font-size: 20px;
+        font-weight: bold;
+        position: absolute;
+        transform: translateY(-50%);
+        color: #fff;
+        top: 50%;
+        left: 0;
+        width: 150px;
+        height: 150px;
+        background-color: #0a1d30;
+        z-index: 30;
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
-      > .line {
-        width: 64px;
-        height: 0;
-        border-top: 3px solid;
-        border-color: #61baf1;
-        margin-top: 7px;
-        margin-bottom: 20px;
-      }
-      > .title {
-        font: 14px/1.6 Arial, "Microsoft YaHei", "黑体", "宋体", sans-serif;
-        font-size: 17px;
-        margin-bottom: 20px;
-      }
-      .el-button {
-        font-size: 15px;
-        border-radius: 0px;
-        border: 2px solid #ffffff;
+      > .sloganWrapper {
+        position: absolute;
+        top: 15%;
+        left: 25%;
         color: #ffffff;
-        background: #0a1d30;
-        padding: 7px 22px;
-        margin-right: 13px;
-        &:hover {
-          cursor: pointer;
-          transition: all 0.3s;
-          color: #35bdf6;
+        font-family: "Libre Baskerville", serif;
+
+        > h1 {
+          font-size: 60px;
+          font-family: "Libre Baskerville", serif;
+        }
+        > .line {
+          width: 64px;
+          height: 0;
+          border-top: 3px solid;
           border-color: #61baf1;
-          box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.5);
+          margin-top: 7px;
+          margin-bottom: 20px;
+        }
+        > .title {
+          font: 14px/1.6 Arial, "Microsoft YaHei", "黑体", "宋体", sans-serif;
+          font-size: 17px;
+          margin-bottom: 20px;
+        }
+        .el-button {
+          font-size: 15px;
+          border-radius: 0px;
+          border: 2px solid #ffffff;
+          color: #ffffff;
+          background: #0a1d30;
+          padding: 7px 22px;
+          margin-right: 13px;
+          &:hover {
+            cursor: pointer;
+            transition: all 0.3s;
+            color: #35bdf6;
+            border-color: #61baf1;
+            box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.5);
+          }
         }
       }
     }
--- src/views/register.vue
+++ src/views/register.vue
@@ -1,15 +1,98 @@
 <template>
-  <div>
-    注册页
+  <div id="login">
+    <div class="loginBox">
+      <h2>注册</h2>
+      <div class="inputBox">
+        <input v-model="username" placeholder="用户名" id="username" />
+      </div>
+      <div class="inputBox">
+        <input v-model="password" type="password" placeholder="密码" @keydown.enter="onRegister" />
+      </div>
+      <el-button size="medium" @click="onRegister">注册</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
-
-}
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    ...mapActions(["register"]),
+    onRegister() {
+      this.register({ username: this.username, password: this.password }).then(
+        () => {
+          this.$router.push({ path: "/" });
+        }
+      );
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#login {
+  display: flex;
+  justify-content: center;
+  .el-button {
+    font-size: 15px;
+    border-radius: 0px;
+    border: 2px solid #415268;
+    color: #415268;
+    background: #ffffff;
+    padding: 7px 22px;
+    margin: 20px;
+    &:hover {
+      cursor: pointer;
+      transition: all 0.3s;
+      color: #4e9d37;
+      border-color: #4e9d37;
+    }
+  }
+  .loginBox {
+    margin-top: 70px;
+    width: 370px;
+    background: #ffffff;
+    box-shadow: 5px 5px 3px rgba(0, 0, 0, 0.5);
+    border: 2px solid #415268;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > .notice {
+      color: #415268;
+      a {
+        text-decoration: none;
+        color: #61baf1;
+      }
+    }
+    > h2 {
+      align-self: center;
+      padding: 20px 0 5px 0;
+      color: #0f1d2e;
+      font-size: 27px;
+    }
+    > .inputBox {
+      > input {
+        outline: none;
+        border: none;
+        border: 2px solid #415268;
+        margin: 10px 0;
+        font-size: 14px;
+        padding: 9px;
+        width: 200px;
+        &:focus {
+          border: 2px solid #61baf1;
+          width: 230px;
+          transition: all 0.4s;
+        }
+      }
+    }
+  }
+}
 </style>
